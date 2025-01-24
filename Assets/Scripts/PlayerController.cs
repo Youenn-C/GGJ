182037871ca1c97cabf028510d6dbe3e9ab255b7
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-01-24 22:20:24 +0100

    [ ADD ] player prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("References"), Space(5)]
     [SerializeField] private GameObject _playerGo;
-    [SerializeField] private Rigidbody _playerRb;
+    [SerializeField] private Rigidbody2D _playerRb;
     
-    [Header("Variables")]
+    [Header("Variables"), Space(5)]
+    [SerializeField] private int _jumpForce;
+    [SerializeField] private int _moveSpeed;
+    [Space(5)]
+    [SerializeField] private bool _isAlive;
+    [SerializeField] private bool _isGrounded;
+    [SerializeField] private bool _asDobleJump;
+    
+    [Header("Rewired"), Space(5)]
+    public Player player;
+    [SerializeField] private int playerId = 0;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +36,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (player.GetButton("RightMovement"))
+        {
+            _playerRb.MovePosition(transform.position + transform.right * _moveSpeed * Time.fixedDeltaTime);
+        }
         
+        if (player.GetButton("LeftMovement"))
+        {
+            _playerRb.MovePosition(transform.position + (-transform.right) * _moveSpeed * Time.fixedDeltaTime);
+        }
+
+        if (player.GetButtonDown("Jump"))
+        {
+            _playerRb.AddForce(transform.up * _jumpForce);
+        }
     }
 }
