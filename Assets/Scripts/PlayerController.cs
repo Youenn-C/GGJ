a8a9d5ee5810f458f522314a53331dbaafb87772
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-01-24 19:09:33 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("References"), Space(5)]
+    [SerializeField] private GameObject _playerGo;
+    [SerializeField] private Rigidbody _playerRb;
+    
+    [Header("Variables")]
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
