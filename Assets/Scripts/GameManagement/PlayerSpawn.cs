b9c5a9f4df3b15a13562ea3bfb8f3f483f5ad46a
CHAPELLIERE Youenn <y.chapelliere@rns.ecolescreatives.com>
2025-01-25 11:59:59 +0100

    [ Update ] fix spawn point

--- Assets/Scripts/GameManagement/PlayerSpawn.cs
+++ Assets/Scripts/GameManagement/PlayerSpawn.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vector2 = System.Numerics.Vector2;
 
 public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _spawnPointPrefab;
     [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Transform _targetTransform;
 
     void Start()
     {
         _playerPrefab = GameObject.FindGameObjectWithTag("Player");
+        _targetTransform.position = _spawnPoint.position;
 
         PlacePlayerOnSpawnPoint();
     }
@@ -16,6 +20,5 @@ public class PlayerSpawn : MonoBehaviour
     void PlacePlayerOnSpawnPoint()
     {
         _playerPrefab.transform.position = _spawnPoint.transform.position;
-        Debug.Log("Player as been placed");
     }
 }
