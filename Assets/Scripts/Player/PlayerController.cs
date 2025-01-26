623c8d93644dd41de7c1d6681d7dba1347a20f18
Game <game@rennes.local>
2025-01-26 11:39:03 +0100

    player animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Rewired;
+using System.Threading.Tasks;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class PlayerController : MonoBehaviour
 
     [Header("References"), Space(5)]
     [SerializeField] private GameObject _playerGo;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Animator _animator;
     public Rigidbody2D _playerRb;
 
     [Header("Variables"), Space(5)]
@@ -23,6 +26,9 @@ public class PlayerController : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     
     public bool canMove = true;
+    private float _timeTillRespawn = 0;
+    private float _respawnTime = 1.0f;
+    private bool _isDying = false;
     
     [Header("Rewired"), Space(5)]
     public Player player;
@@ -31,6 +37,20 @@ public class PlayerController : MonoBehaviour
     public bool IsAlive()
     {
         return _isAlive;
+    }
+    public void SetIsAlive(bool value)
+    {
+        if (value)
+        {
+            _animator.SetBool("IsAlive", value);
+            _isAlive = value;
+        }
+        else
+        {
+            _animator.SetBool("IsAlive", false);
+            _isDying = true;
+            _timeTillRespawn = 0;
+        }
     }
 
     void Awake()
@@ -50,6 +70,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MovePlayer();
+
+        // Die duration
+        if (_isDying)
+        {
+            if (_timeTillRespawn < _respawnTime)
+            {
+                _timeTillRespawn += Time.deltaTime;
+            }
+            else
+            {
+                _isAlive = false;
+                _timeTillRespawn = 0;
+                _isDying = false;
+            }
+        }
     }
 
     void MovePlayer()
@@ -57,7 +92,9 @@ public class PlayerController : MonoBehaviour
         if (canMove)
         {
             float horizontalMovement = player.GetAxis("HorizontalMovement") * _moveSpeed * Time.deltaTime;
-        
+            FlipPlayer(horizontalMovement);
+            _animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
             Vector3 targetVelocity = new Vector2(horizontalMovement, _playerRb.velocity.y);
             _playerRb.velocity = Vector3.SmoothDamp(_playerRb.velocity, targetVelocity, ref velocity, 0.05f);
                 
@@ -68,32 +105,53 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void FlipPlayer(float horizontalMovement)
+    {
+        bool flipX = _spriteRenderer.flipX;
+        if (flipX && horizontalMovement > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if(!flipX && horizontalMovement < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+
+    void SetIsGrounded(bool value)
+    {
+        _isGrounded = value;
+        _animator.SetBool("IsGrounded", value);
+    }
+
     public void Jump(float jumpForce)
     {
+        _animator.SetTrigger("IsJumping");
         Debug.Log(transform.up * jumpForce);
         _playerRb.AddForce(transform.up * jumpForce);
-        _isGrounded = false;
+        SetIsGrounded(false);
     }
 
     public void JumpWithAngle(float jumpForce, float jumpAngle)
     {
+        _animator.SetTrigger("IsJumping");
         float angleInRadians = jumpAngle * Mathf.PI / 180.0f;
         Vector2 direction = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
         Debug.Log(direction * jumpForce);
         _playerRb.AddForce(direction * jumpForce);
-        _isGrounded = false;
+        SetIsGrounded(false);
     }
 
     public void Die()
     {
         Debug.Log("Player died");
         // TODO: run dead animation
-        _isAlive = false;
+        SetIsAlive(false);
     }
 
     public void Respawn()
     {
-        _isAlive = true;
+        SetIsAlive(true);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -102,7 +160,7 @@ public class PlayerController : MonoBehaviour
         {
             if (collision.gameObject.CompareTag("Ground"))
             {
-                _isGrounded = true;
+                SetIsGrounded(true);
             }
         }
     }
